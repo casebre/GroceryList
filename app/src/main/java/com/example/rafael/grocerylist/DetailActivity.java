3417286436613d564d59f8dde6cea85aff01ef2d
Rafael Casale Abe <rafael.casaleabe@gmail.com>
2015-12-21 11:54:17 -0500

    Creating a list (hard coded) and links between activities

---
+++ app/src/main/java/com/example/rafael/grocerylist/DetailActivity.java
@@ -0,0 +1,47 @@
+package com.example.rafael.grocerylist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class DetailActivity extends AppCompatActivity {
+
+    public void showLists(View view) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent i = getIntent();
+        int idList = i.getIntExtra("List",-1);
+
+        if (idList != -1)
+            Log.i("", "Failed");
+        else
+            Log.i("", "List found!");
+
+
+    }
+
+}
